4acbaeb24498bd0bb00923b47f8e118b253dd8e3
Aleksandra Cheidze <a.cxeidze@gmail.com>
2023-07-29 13:39:18 +0200

    feat: create file dict.txt

--- Lesson52/Homework52/src/Dictionary.java
+++ Lesson52/Homework52/src/Dictionary.java
@@ -1,3 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Dictionary {
   // Программисты, как вы уже знаете, постоянно учатся, а в общении между собой используют весьма
   // специфический язык. Чтобы систематизировать ваш пополняющийся профессиональный лексикон, мы
@@ -12,8 +18,54 @@ public class Dictionary {
   // необходимо вывести на экран его определение.
   // Если слова в словаре нет, программа должна вывести "Не найдено", без кавычек.
 
-  public static void main(String[] args) {
+  private static final String SEPARATOR = ": ";
 
+  public static void main(String[] args) {
+    // Чтение словаря из файла
+    Map<String, String> dictionary = readDictionaryFromFile("res/dict.txt");
+
+    // Ввод количества поисковых слов
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Введите количество поисковых слов: ");
+    int m = Integer.parseInt(scanner.nextLine());
+
+    // Поиск и вывод определения для каждого слова
+    for (int i = 0; i < m; i++) {
+      System.out.print("Введите слово для поиска: ");
+      String word = scanner.nextLine()
+          .toLowerCase(); // Приводим слово к нижнему регистру для независимого поиска
+      if (dictionary.containsKey(word)) {
+        System.out.println(dictionary.get(word));
+      } else {
+        System.out.println("Не найдено");
+      }
+    }
+    scanner.close(); // Don't forget to close the scanner after use
   }
 
+  private static Map<String, String> readDictionaryFromFile(String filename) {
+    Map<String, String> dictionary = new HashMap<>();
+    Scanner scanner;
+    try {
+      scanner = new Scanner(new File("res/dict.txt"));
+      int n = Integer.parseInt(scanner.nextLine()); // Считываем количество слов в словаре
+      for (int i = 0; i < n; i++) {
+        String line = scanner.nextLine();
+        String[] parts = line.split(
+            SEPARATOR); // Разделяем строку на слово и определение с помощью сепаратора
+        if (parts.length == 2) {
+          String word = parts[0].toLowerCase(); // Приводим слово к нижнему регистру для независимого поиска
+          String definition = parts[1];
+          dictionary.put(word, definition); // Добавляем слово и его определение в словарь
+        }
+      }
+      scanner.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("Файл не найден: " + e);
+    }
+    return dictionary;
+  }
 }
+
+
+
