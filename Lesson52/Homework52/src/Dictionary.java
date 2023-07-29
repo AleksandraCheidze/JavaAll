8d2f28045e2bf86665aa88b01f02195dec3b9feb
Aleksandra Cheidze <a.cxeidze@gmail.com>
2023-07-29 16:32:29 +0200

    feat: add Map

--- Lesson52/Homework52/src/Dictionary.java
+++ Lesson52/Homework52/src/Dictionary.java
@@ -24,6 +24,7 @@ public class Dictionary {
     // Чтение словаря из файла
     Map<String, String> dictionary = readDictionaryFromFile("res/dict.txt");
 
+
     // Ввод количества поисковых слов
     Scanner scanner = new Scanner(System.in);
     System.out.print("Введите количество поисковых слов: ");
