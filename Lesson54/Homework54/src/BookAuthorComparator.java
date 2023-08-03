bd7ff88d679966c14ecfa0c24dfa87f0d06d6243
Aleksandra Cheidze <a.cxeidze@gmail.com>
2023-08-03 13:38:48 +0200

    feat: implement BookAuthorComparator

--- Lesson54/Homework54/src/BookAuthorComparator.java
+++ Lesson54/Homework54/src/BookAuthorComparator.java
@@ -1,4 +1,14 @@
-public class BookAuthorComparator implements Comparable<Book> {
+import java.util.Comparator;
+
+public class BookAuthorComparator implements Comparator<Book> {
+
+  public int compare(Book o1, Book o2) {
+    int compareTitle = o1.getTitle().compareTo(o2.getTitle());
+    if (compareTitle != 0) {
+      return compareTitle;
+    }
+    return o1.getAuthor().compareTo(o2.getAuthor());
+  }
 
 
 }
